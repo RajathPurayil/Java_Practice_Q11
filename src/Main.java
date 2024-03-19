fc97bd4e808cc4cf6bf5e58f4f587154c8400dc8
Rajath12e <38193362+Rajath12e@users.noreply.github.com>
2024-03-18 23:35:34 -0400

    Updated Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      //  Write a Java Program to Compute the Sum of Array Elements.
+
+        // Declaration of Array
+
+        int arr[] ={12,12,4,15,5};
+
+        // method for sumof elements in an Array
+
+            static  int sum()
+
+        {
+
+            sum =0; //Initialize sum
+            int i;
+
+            // iterate through all elements  and add them to sum
+
+            for(i=0;i<arr.length ; i++)
+                sum+=arr[i];
+
+            return sum;
+
+
+        }
+
+
+
     }
 }
